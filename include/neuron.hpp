f7953e12a3bf529aeecf12ee3f0dc6beb429c50e
Alex Koziell <alexkoziell@gmail.com>
2019-08-23 23:33:33 +0100

    Implemented CMake and started Neuron class.

---
+++ include/neuron.hpp
@@ -0,0 +1,32 @@
+#ifndef _NEURON_HPP_
+#define _NEURON_HPP_
+
+#include <iostream>
+using namespace std;
+
+class Neuron {
+    public:
+        Neuron(double val);
+
+        // fast sigmoid funtion
+        // f(x) = x / (1 + |x|)
+        // derivative f(x) * (1 - f(x))
+        void activate();
+        void derive();
+
+        double getVal() { return this->val; }
+        double getActivatedVal() { return this->activatedVal; }
+        double getDerivedVal() { return this->derivedVal; }
+
+    private:
+        // absolute value
+        double val;
+
+        // activation (between 0 and 1)
+        double activatedVal;
+
+        // approximate derivative of the activated value
+        double derivedVal;
+};
+
+#endif
