4a5bee06c7b792da3cf22d293608a43864284d09
Alex Koziell <alexkoziell@gmail.com>
2019-09-20 21:24:26 +0100

    Network, layer and neuron outlines ready to start implementing feedforward.

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 using namespace std;
 
 int main() {
+
+    /* topology: contains the number of neurons in each layer. */
     vector<int> topology;
     topology.push_back(3);
     topology.push_back(2);
     topology.push_back(3);
 
+    /* input: the starting values of the input neurons. */
     vector<double> input;
     input.push_back(1.0);
     input.push_back(0.0);
@@ -24,7 +27,20 @@ int main() {
     Network *neuralNetwork = new Network(topology);
 
     cout << "NUMBER OF LAYERS: " << topology.size() << endl;
-    cout << "NUMBER OF INPUT NEURONS: " << topology.at(0) << endl << endl;
+    cout << "NUMBER OF INPUT NEURONS: " << topology.at(0) << endl;
+    cout << "NUMBER OF OUTPUT NEURONS: " << topology.back() << endl << endl;
+
+    for (int layerNum=0; layerNum<topology.size(); layerNum++) {
+        cout << "Layer " << layerNum << ':';
+        cout << setw(15-topology.at(layerNum)*2.5);
+        for (int neuronNum=0; neuronNum<topology.at(layerNum); neuronNum++) {
+            cout << 'o' << setw(5) << ' ';
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+
     neuralNetwork->setInput(input);
 
     neuralNetwork->printToConsole();
